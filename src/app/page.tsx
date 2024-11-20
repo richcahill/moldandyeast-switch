641d92c714948ec037d1cd55ce233efb5f3de5f0
Rich Cahill <rich@rc3.me>
2024-11-20 10:02:23 +0000

    add switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,100 @@
 "use client";
 
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { littleThings, theRot } from "@/lib/copy";
+import { motion, AnimatePresence } from "motion/react";
 
 type LittleOrRot = "little" | "rot";
 
 const Poem = ({ littleOrRot }: { littleOrRot: LittleOrRot }) => {
   return (
-    <>
+    <AnimatePresence mode="wait">
       {littleOrRot === "little"
         ? littleThings.map((item, index) => (
-            <div key={index}>
+            <motion.div
+              key={`little-${index}`}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }}
+              transition={{ duration: 0.6 }}
+            >
               {item.type === "h" && (
-                <h1 className="text-white text-2xl mb-4">{item.content}</h1>
+                <h1 className="text-2xl mb-4">{item.content}</h1>
               )}
               {item.type === "p" && (
-                <p className="text-white max-w-prose mb-1">{item.content}</p>
+                <p className="max-w-prose mb-1">{item.content}</p>
               )}
-            </div>
+            </motion.div>
           ))
         : theRot.map((item, index) => (
-            <div key={index}>
+            <motion.div
+              key={`rot-${index}`}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }}
+              transition={{ duration: 0.6 }}
+            >
               {item.type === "h" && (
-                <h1 className="text-white text-2xl mb-4">{item.content}</h1>
+                <h1 className="text-2xl mb-4">{item.content}</h1>
               )}
               {item.type === "p" && (
-                <p className="text-white max-w-prose mb-1">{item.content}</p>
+                <p className="max-w-prose mb-1">{item.content}</p>
               )}
-            </div>
+            </motion.div>
           ))}
-    </>
+    </AnimatePresence>
+  );
+};
+
+const Switch = ({
+  setLittleOrRot,
+  littleOrRot,
+}: {
+  setLittleOrRot: (littleOrRot: LittleOrRot) => void;
+  littleOrRot: LittleOrRot;
+}) => {
+  return (
+    <div
+      className={`flex relative p-1 rounded-full transition-all duration-1000 ${
+        littleOrRot === "little"
+          ? "bg-white text-black border border-black"
+          : ""
+      } ${
+        littleOrRot === "rot"
+          ? "bg-[#ea3323] text-black border border-[#ea3323]"
+          : ""
+      }`}
+    >
+      <motion.div
+        className="absolute top-1 bottom-1 z-0 bg-black rounded-full"
+        initial={false}
+        animate={{
+          x: littleOrRot === "little" ? 0 : "100%",
+          width: "128px",
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+        }}
+      />
+      <button
+        onClick={() => setLittleOrRot("little")}
+        className={`flex-1 px-6 py-2 flex justify-center w-32 relative ${
+          littleOrRot === "little" ? "text-white" : ""
+        }`}
+      >
+        <div className="relative z-10">Light Mode</div>
+      </button>
+      <button
+        onClick={() => setLittleOrRot("rot")}
+        className={`flex-1 px-6 py-2 flex justify-center w-32 relative ${
+          littleOrRot === "rot" ? "text-[#ea3323]" : ""
+        }`}
+      >
+        <div className="relative z-10">Dark Mode</div>
+      </button>
+    </div>
   );
 };
 
@@ -37,15 +102,18 @@ export default function Home() {
   const [littleOrRot, setLittleOrRot] = useState<LittleOrRot>("little");
 
   return (
-    <div className="h-screen w-screen overflow-y-scroll p-6">
+    <div className="h-screen w-screen overflow-y-scroll p-6 bg-black text-sm font-sans">
       <div className="flex flex-col gap-4 max-w-3xl mx-auto h-full">
-        <div className="bg-red-700 h-full flex-1 p-8 rounded">
+        <div
+          className={`h-full flex-1 transition-all duration-1000 p-8 rounded ${
+            littleOrRot === "little" ? "bg-white text-black" : ""
+          } ${littleOrRot === "rot" ? "bg-black text-[#ea3323]" : ""}`}
+        >
           <Poem littleOrRot={littleOrRot} />
         </div>
       </div>
-      <div className="flex justify-center gap-4 fixed bottom-10 w-full">
-        <button onClick={() => setLittleOrRot("little")}>Little</button>
-        <button onClick={() => setLittleOrRot("rot")}>Rot</button>
+      <div className="flex justify-center gap-4 fixed bottom-16 w-full">
+        <Switch setLittleOrRot={setLittleOrRot} littleOrRot={littleOrRot} />
       </div>
     </div>
   );
